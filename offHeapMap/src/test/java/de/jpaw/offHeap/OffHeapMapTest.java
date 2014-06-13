2777e8b346cd91c234d30a1d76096b7478365591
Michael Bischoff <jpaw@online.de>
2014-06-13 16:49:02 +0200

    adding more tests

--- offHeapMap/src/test/java/de/jpaw/offHeap/OffHeapMapTest.java
+++ offHeapMap/src/test/java/de/jpaw/offHeap/OffHeapMapTest.java
@@ -1,6 +1,8 @@
 package de.jpaw.offHeap;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.testng.annotations.Test;
 
@@ -33,23 +35,47 @@ public class OffHeapMapTest {
         myMap.close();
     }
 
-//    public void runStoreRetrieveTestCompressed() {
-//        LongToByteArrayOffHeapMap myMap = new LongToByteArrayOffHeapMap(1000);
-//        myMap.setMaxUncompressedSize(0);
-//        byte [] oldData = TEXT.getBytes();
-//        myMap.set(KEY, oldData);
-//        
-//        assert(myMap.get(KEY - 1L) == null);
-//        assert(myMap.get(KEY + 1L) == null);
-//        assert(myMap.get(KEY) != null);
-//        byte [] newData = myMap.get(KEY);
-//        assert(newData.length == oldData.length);
-//        String newText = new String(newData);
-//        System.out.println(TEXT);
-//        System.out.println(newText);
-//        assert(Arrays.equals(newData, oldData));
-//        
-//        myMap.close();
-//    }
+    public void runStoreRetrieveTestCompressed() {
+        LongToByteArrayOffHeapMap myMap = new LongToByteArrayOffHeapMap(1000);
+        myMap.setMaxUncompressedSize(0);
+        byte [] oldData = TEXT.getBytes();
+        myMap.set(KEY, oldData);
+        
+        assert(myMap.get(KEY - 1L) == null);
+        assert(myMap.get(KEY + 1L) == null);
+        assert(myMap.get(KEY) != null);
+        byte [] newData = myMap.get(KEY);
+        assert(newData.length == oldData.length);
+        String newText = new String(newData);
+        System.out.println(TEXT);
+        System.out.println(newText);
+        assert(Arrays.equals(newData, oldData));
+        
+        myMap.close();
+    }
+
+    public void runStoreRetrieveTestCompressedWithVerify() {
+        final int HASH_SIZE = 10000;
+        LongToByteArrayOffHeapMap myMap = new LongToByteArrayOffHeapMap(HASH_SIZE);
+        myMap.setMaxUncompressedSize(0);
+        
+        final int MAX_ENTRIES = 3 * HASH_SIZE; 
+        final List<byte []> myMem = new ArrayList<byte []>(MAX_ENTRIES);
+        
+        // pick a size such that we have to have multiple entries per hash map slot
+        // compute and store the entries
+        for (int i = 0; i < MAX_ENTRIES; ++i) {
+            byte [] data = (TEXT + i).getBytes();
+            myMem.add(data);
+            myMap.set((long)i, data);
+        }
+        // retrieve and compare them
+        for (int i = 0; i < MAX_ENTRIES; ++i) {
+            byte [] data = myMap.get((long)i);
+            assert(Arrays.equals(data, myMem.get(i)));
+        }
+        System.out.println("Compare test successful");
+        myMap.close();
+    }
 
 }
